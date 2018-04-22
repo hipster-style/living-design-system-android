cafe5e4daca2e71da52eb7bc53b8d00bbab065c3
Jolanda Verhoef <jolanda.verhoef@coolblue.nl>
2018-04-22 12:30:30 +0200

    Add generated design tokens and update button UI

--- library/src/main/java/com/jolandaverhoef/designsystem/library/Button.kt
+++ library/src/main/java/com/jolandaverhoef/designsystem/library/Button.kt
@@ -2,6 +2,7 @@ package com.jolandaverhoef.designsystem.library
 
 import android.content.Context
 import android.support.constraint.ConstraintLayout
+import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.View
 import kotlinx.android.synthetic.main.merge_button.view.*
@@ -20,5 +21,7 @@ class Button @JvmOverloads constructor(
         button_image.setImageResource(
                 if(active) android.R.drawable.ic_btn_speak_now
                 else android.R.drawable.ic_delete)
+        button_text.setTextColor(ContextCompat.getColor(context, R.color.COLOR_TEXT_BUTTON))
+        setBackgroundResource(R.drawable.bg_button)
     }
 }
